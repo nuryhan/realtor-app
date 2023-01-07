3e9830ebc68718319e07807f1819f05169ee2bd7
nuryhan <nurmuhammet.muradov0309@gmail.com>
2023-01-07 11:55:47 +0500

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Header = () => {
+  const location = useLocation();
+
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50 ">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto ">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="realtor"
+            className="h-5 cursor-pointer "
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500 "
+              } `}
+            >
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500 "
+              } `}
+            >
+              <Link to="/offers">Offers</Link>
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "text-black border-b-red-500 "
+              } `}
+            >
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
