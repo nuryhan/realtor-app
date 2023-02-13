8caa0a0cfcbe051af1b9067291088a2ea2166ee9
nuryhan <nurmuhammet.muradov0309@gmail.com>
2023-02-13 21:00:37 +0500

    complete the places for rent and sales

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -32,6 +33,8 @@ function App() {
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
